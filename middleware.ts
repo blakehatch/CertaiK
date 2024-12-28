ff0bb1cb9210c95bdb5bfee0dcf39ae1f3679794
Peter Simone <plssimone@gmail.com>
2024-12-28 00:53:01 -0500

    update og, remove middleware

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,7 @@ export function middleware(req: NextRequest) {
   // Only expose the api to requests from the app.
   const url = req.nextUrl;
   const { pathname } = url;
+  return NextResponse.next();
   if (pathname.startsWith(`/api/`)) {
     if (!req.headers.get("referer")?.includes(process.env.VERCEL_URL as string)) {
       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
